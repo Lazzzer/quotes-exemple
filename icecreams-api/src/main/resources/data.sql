ecec40eb21523c5d4643fa82ceed4689b01ee911
Lazzzer <dev@lazarpavicevic.ch>
2023-01-26 23:45:29 +0100

    refactor: better data.sql file

--- icecreams-api/src/main/resources/data.sql
+++ icecreams-api/src/main/resources/data.sql
@@ -1,12 +1,30 @@
-insert into origins values (default,'Italie');
 insert into origins values (default,'Suisse');
+insert into origins values (default,'Italie');
+insert into origins values (default,'Royaume-Uni');
+insert into origins values (default,'Etats-Unis');
 insert into origins values (default,'Japon');
-insert into origins values (default,'Argentine');
 
-insert into ice_creams values (default,'Solero', 3.5, 1);
-insert into ice_creams values (default,'Extrême Chocolat', 4, 2);
-insert into ice_creams values (default,'Magnum Caramel', 4.5, 2);
+insert into ice_creams values (default,'Solero', 6.7, 3);
+insert into ice_creams values (default,'Extrême Chocolat', 8.95, 1);
+insert into ice_creams values (default,'Magnum Double Gold Caramel', 8.95, 3);
+insert into ice_creams values (default,'Mövenpick Strawberry', 11.95, 1);
+insert into ice_creams values (default,'Gelato Stracciatella', 5.95, 2);
+insert into ice_creams values (default,'Ben & Jerrys Netflix & Chilld', 9.95, 4);
+insert into ice_creams values (default,'Choquello', 6.95, 1);
 
 insert into containers values (default,'Cornet');
 insert into containers values (default,'Pot');
-insert into containers values (default,'Giga cornet sa mere');
+insert into containers values (default,'Bâtonnet');
+
+insert into ice_creams_containers values (1,3);
+insert into ice_creams_containers values (2,1);
+insert into ice_creams_containers values (3,2);
+insert into ice_creams_containers values (3,3);
+insert into ice_creams_containers values (4,1);
+insert into ice_creams_containers values (4,2);
+insert into ice_creams_containers values (5,1);
+insert into ice_creams_containers values (5,2);
+insert into ice_creams_containers values (5,3);
+insert into ice_creams_containers values (6,2);
+insert into ice_creams_containers values (7,3);
+
