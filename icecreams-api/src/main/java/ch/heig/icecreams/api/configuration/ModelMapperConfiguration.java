d5b32284138c314d56be28f7a86d0fee54d637f0
Lazzzer <dev@lazarpavicevic.ch>
2023-01-27 00:08:44 +0100

    style: converters

--- icecreams-api/src/main/java/ch/heig/icecreams/api/configuration/ModelMapperConfiguration.java
+++ icecreams-api/src/main/java/ch/heig/icecreams/api/configuration/ModelMapperConfiguration.java
@@ -1,4 +1,4 @@
-// Source : https://stackoverflow.com/questions/65807229/how-can-i-use-the-modelmapper-in-spring
+// Sources : https://stackoverflow.com/questions/65807229/how-can-i-use-the-modelmapper-in-spring
 
 package ch.heig.icecreams.api.configuration;
 
@@ -21,14 +21,18 @@ public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         // Création des converters pour les relations ManyToMany
-        Converter<List<Integer>, List<ContainerEntity>> idToContainer = ctx -> ctx.getSource().stream()
+        Converter<List<Integer>, List<ContainerEntity>> idToContainer = ctx -> ctx
+                .getSource()
+                .stream()
                 .map(id -> {
                     var container = new ContainerEntity();
                     container.setId(id);
                     return container;
                 })
                 .toList();
-        Converter<List<Integer>, List<IceCreamEntity>> idToIceCream = ctx -> ctx.getSource().stream()
+        Converter<List<Integer>, List<IceCreamEntity>> idToIceCream = ctx -> ctx
+                .getSource()
+                .stream()
                 .map(id -> {
                     var iceCream = new IceCreamEntity();
                     iceCream.setId(id);
@@ -41,18 +45,24 @@ public class ModelMapperConfiguration {
         // Ajout du mapping pour la relation OneToMany
         modelMapper
                 .typeMap(OriginDTOid.class, OriginEntity.class)
-                .addMappings(mapper -> mapper.using(idToIceCream)
-                        .map(OriginDTOid::getIceCreamIds, OriginEntity::setIceCreams));
+                .addMappings(mapper -> mapper
+                        .using(idToIceCream)
+                        .map(OriginDTOid::getIceCreamIds, OriginEntity::setIceCreams)
+                );
 
         // Ajout des mappings pour les relations ManyToMany
         modelMapper
                 .typeMap(IceCreamDTOid.class, IceCreamEntity.class)
-                .addMappings(mapper -> mapper.using(idToContainer)
-                        .map(IceCreamDTOid::getContainerIds, IceCreamEntity::setContainers));
+                .addMappings(mapper -> mapper
+                        .using(idToContainer)
+                        .map(IceCreamDTOid::getContainerIds, IceCreamEntity::setContainers)
+                );
         modelMapper
                 .typeMap(ContainerDTOid.class, ContainerEntity.class)
-                .addMappings(mapper -> mapper.using(idToIceCream)
-                        .map(ContainerDTOid::getIceCreamIds, ContainerEntity::setIceCreams));
+                .addMappings(mapper -> mapper
+                        .using(idToIceCream)
+                        .map(ContainerDTOid::getIceCreamIds, ContainerEntity::setIceCreams)
+                );
         return modelMapper;
     }
 }
