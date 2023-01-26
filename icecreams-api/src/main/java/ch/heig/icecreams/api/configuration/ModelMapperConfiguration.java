953db5f40b88a93b79f4b7c163abe9f3a987fb71
Lazzzer <dev@lazarpavicevic.ch>
2023-01-27 00:23:41 +0100

    refactor: cleaner converters

--- icecreams-api/src/main/java/ch/heig/icecreams/api/configuration/ModelMapperConfiguration.java
+++ icecreams-api/src/main/java/ch/heig/icecreams/api/configuration/ModelMapperConfiguration.java
@@ -1,9 +1,11 @@
+// Sources : https://www.baeldung.com/java-modelmapper-lists
+
 package ch.heig.icecreams.api.configuration;
 
 import ch.heig.icecreams.api.entities.ContainerEntity;
 import ch.heig.icecreams.api.entities.IceCreamEntity;
 import ch.heig.icecreams.api.entities.OriginEntity;
-import org.modelmapper.Converter;
+import org.modelmapper.AbstractConverter;
 import org.modelmapper.ModelMapper;
 import org.openapitools.model.ContainerDTOid;
 import org.openapitools.model.IceCreamDTOid;
@@ -16,35 +18,44 @@ import java.util.List;
 @Configuration
 public class ModelMapperConfiguration {
 
+    // Création des converters pour les relations ManyToMany où nous créons un objet à partir de l'id
+    static class IdsContainersConverter extends AbstractConverter<List<Integer>, List<ContainerEntity>> {
+        @Override
+        protected List<ContainerEntity> convert(List<Integer> containerIds) {
+            return containerIds
+                    .stream()
+                    .map(id -> {
+                        var container = new ContainerEntity();
+                        container.setId(id);
+                        return container;
+                    })
+                    .toList();
+        }
+    }
+
+    static class IdsIceCreamsConverter extends AbstractConverter<List<Integer>, List<IceCreamEntity>> {
+        @Override
+        protected List<IceCreamEntity> convert(List<Integer> iceCreamIds) {
+            return iceCreamIds
+                    .stream()
+                    .map(id -> {
+                        var iceCream = new IceCreamEntity();
+                        iceCream.setId(id);
+                        return iceCream;
+                    })
+                    .toList();
+        }
+    }
+
     @Bean
     public ModelMapper modelMapper() {
-        // Création des converters pour les relations ManyToMany
-        Converter<List<Integer>, List<ContainerEntity>> idToContainer = ctx -> ctx
-                .getSource()
-                .stream()
-                .map(id -> {
-                    var container = new ContainerEntity();
-                    container.setId(id);
-                    return container;
-                })
-                .toList();
-        Converter<List<Integer>, List<IceCreamEntity>> idToIceCream = ctx -> ctx
-                .getSource()
-                .stream()
-                .map(id -> {
-                    var iceCream = new IceCreamEntity();
-                    iceCream.setId(id);
-                    return iceCream;
-                })
-                .toList();
-
         var modelMapper = new ModelMapper();
 
         // Ajout du mapping pour la relation OneToMany
         modelMapper
                 .typeMap(OriginDTOid.class, OriginEntity.class)
                 .addMappings(mapper -> mapper
-                        .using(idToIceCream)
+                        .using(new IdsIceCreamsConverter())
                         .map(OriginDTOid::getIceCreamIds, OriginEntity::setIceCreams)
                 );
 
@@ -52,15 +63,16 @@ public class ModelMapperConfiguration {
         modelMapper
                 .typeMap(IceCreamDTOid.class, IceCreamEntity.class)
                 .addMappings(mapper -> mapper
-                        .using(idToContainer)
+                        .using(new IdsContainersConverter())
                         .map(IceCreamDTOid::getContainerIds, IceCreamEntity::setContainers)
                 );
         modelMapper
                 .typeMap(ContainerDTOid.class, ContainerEntity.class)
                 .addMappings(mapper -> mapper
-                        .using(idToIceCream)
+                        .using(new IdsIceCreamsConverter())
                         .map(ContainerDTOid::getIceCreamIds, ContainerEntity::setIceCreams)
                 );
+
         return modelMapper;
     }
 }
